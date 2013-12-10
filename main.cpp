612616cdff815439487e317f0eb5f7de524d1f35
Crom (Thibaut CHARLES) <crom29@hotmail.fr>
2013-12-10 11:33:00 +0100

    Added FreeRTOS + sample program

--- main.cpp
+++ main.cpp
@@ -1,112 +1,43 @@
 #include <optimizecpp.cpp>
-//
-//#include "STM32Lib.hpp"
-//
-////	B
-////R		G
-////	O
-//
-//int main()
-//{
-//	STM32* stm = STM32::Get();
-//	stm->SetPinDigitalValue(STM32::PinLedGreen, true);
-//	stm->SetPinDigitalValue(STM32::PinLedBlue, true);
-//	stm->SetPinDigitalValue(STM32::PinLedOrange, true);
-//	stm->SetPinDigitalValue(STM32::PinLedRed, true);
-//
-////	while(1){
-////		if(stm->GetPinDigitalValue(STM32::PinButton))
-////			stm->SetPinDigitalValue(STM32::PinLedGreen, true);
-////		else
-////			stm->SetPinDigitalValue(STM32::PinLedGreen, false);
-////
-////	}
-////	stm->ExitWithCode("rgboobgr");
-//	return 0;
-//}
-
-//#include "STM32Lib.hpp"
-//
-//int main()
-//{
-//	STM32* stm = STM32::Get();
-//
-//	stm->PlayCode("rogb");
-//	while(1)
-//	{
-//		Vector3D<int8_t> vAcc(stm->GetAccelerometerValue());
-//
-//
-//		if(!stm->GetPinDigitalValue(STM32::PinButton))
-//		{
-//			if(vAcc.x>0)
-//			{
-//				stm->SetPinDigitalValue(STM32::PinLedBlue, true);
-//				stm->SetPinDigitalValue(STM32::PinLedOrange, false);
-//			}
-//			else if(vAcc.x<0)
-//			{
-//				stm->SetPinDigitalValue(STM32::PinLedBlue, false);
-//				stm->SetPinDigitalValue(STM32::PinLedOrange, true);
-//			}
-//			else
-//			{
-//				stm->SetPinDigitalValue(STM32::PinLedBlue, false);
-//				stm->SetPinDigitalValue(STM32::PinLedOrange, false);
-//			}
-//
-//			if(vAcc.y>0)
-//			{
-//				stm->SetPinDigitalValue(STM32::PinLedRed, true);
-//				stm->SetPinDigitalValue(STM32::PinLedGreen, false);
-//			}
-//			else if(vAcc.y<0)
-//			{
-//				stm->SetPinDigitalValue(STM32::PinLedRed, false);
-//				stm->SetPinDigitalValue(STM32::PinLedGreen, true);
-//			}
-//			else
-//			{
-//				stm->SetPinDigitalValue(STM32::PinLedRed, false);
-//				stm->SetPinDigitalValue(STM32::PinLedGreen, false);
-//			}
-//		}
-//		else
-//		{
-//			stm->SetPinDigitalValue(STM32::PinLedGreen, false);
-//			stm->SetPinDigitalValue(STM32::PinLedRed, false);
-//			if(vAcc.z>0)
-//			{
-//				stm->SetPinDigitalValue(STM32::PinLedBlue, true);
-//				stm->SetPinDigitalValue(STM32::PinLedOrange, false);
-//			}
-//			else if(vAcc.z<0)
-//			{
-//				stm->SetPinDigitalValue(STM32::PinLedBlue, false);
-//				stm->SetPinDigitalValue(STM32::PinLedOrange, true);
-//			}
-//			else
-//			{
-//				stm->SetPinDigitalValue(STM32::PinLedBlue, false);
-//				stm->SetPinDigitalValue(STM32::PinLedOrange, false);
-//			}
-//
-//		}
-//	}
-//	stm->ExitWithCode("ror");
-//	return 0;
-//}
-//
-//
 #include "STM32Lib.hpp"
+#include <FreeRTOS.h>
+#include <task.h>
+#include <queue.h>
+
+void BlinkBlue(void* param)
+{
+	while(1)
+	{
+		STM32::Get()->SetPinDigitalValue(STM32::PinLedBlue, true);
+		vTaskDelay(500);
+		STM32::Get()->SetPinDigitalValue(STM32::PinLedBlue, false);
+		vTaskDelay(500);
+	}
+	vTaskDelete(0);
+}
+void BlinkRed(void* param)
+{
+	vTaskDelay(250);
+	while(1)
+	{
+		STM32::Get()->SetPinDigitalValue(STM32::PinLedRed, true);
+		vTaskDelay(500);
+		STM32::Get()->SetPinDigitalValue(STM32::PinLedRed, false);
+		vTaskDelay(500);
+	}
+	vTaskDelete(0);
+}
 
 int main(void)
 {
 	STM32* stm = STM32::Get();
-	stm->SetTim1PWM(1, 0.9);
-	stm->SetTim1PWM(2, 0.6);
-	stm->SetTim1PWM(3, 0.3);
-	stm->SetTim1PWM(4, 0.1);
+
+	xTaskCreate(BlinkBlue, (const signed char*)"Coucou", 240, nullptr, 1, NULL);
+	xTaskCreate(BlinkRed, (const signed char*)"Coucou", 240, nullptr, 1, NULL);
+
+	vTaskStartScheduler();
+
+	while(1);
 }
 
 
